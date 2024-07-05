f696baf9beb060903a693a7790e61fc4955d960f
Nguyễn Trường Thịnh <100020360+thinhsama@users.noreply.github.com>
2024-07-05 21:05:05 +0700

    label noise

--- DSap1.py
+++ DSap1.py
@@ -2,7 +2,8 @@
 #______________________________________PEP8____________________________________
 #_______________________________________________________________________
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import tensorflow as tf
@@ -637,7 +638,114 @@ class DShap(object):
                 self.directory, 'plots', '{}.png'.format(name)),
                         bbox_inches = 'tight')
             plt.close()
-            
+    
+    def performance_plots_poison(self, vals, name=None, 
+                          num_plot_markers=20, sources=None, noise_mask=None):
+        """Plots the effect of removing valuable points.
+        
+        Args:
+            vals: A list of different valuations of data points each
+                 in the format of an array in the same length of the data.
+            name: Name of the saved plot if not None.
+            num_plot_markers: number of points in each plot.
+            sources: If values are for sources of data points rather than
+                   individual points. In the format of an assignment array
+                   or dict.
+                   
+        Returns:
+            Plots showing the change in performance as points are removed
+            from most valuable to least.
+        """
+        # plt.rcParams['figure.figsize'] = 8,8
+        # plt.rcParams['font.size'] = 25
+        # plt.xlabel('Fraction of train data removed (%)')
+        # plt.ylabel('Prediction accuracy (%)', fontsize=20)
+        if not isinstance(vals, list) and not isinstance(vals, tuple):
+            vals = [vals]
+        if sources is None:
+            sources = {i:np.array([i]) for i in range(len(self.X))}
+        elif not isinstance(sources, dict):
+            sources = {i:np.where(sources==i)[0] for i in set(sources)}
+        vals_sources = [np.array([np.sum(val[sources[i]]) 
+                                  for i in range(len(sources.keys()))])
+                  for val in vals]
+        #noise_sorted_indices = np.argsort(vals_sources[0])[::-1]
+        noise_sorted_indices = np.argsort(vals_sources[0])
+        print(noise_sorted_indices.shape)
+        print(noise_sorted_indices)
+        #sorted_noise_indices = np.where(noise_mask[noise_sorted_indices] == 1)[0]
+        #print(sorted_noise_indices.shape)
+        #print(sorted_noise_indices)
+        self.visualize_values_distr_sorted(noise_sorted_indices, trsize = len(self.X), portion=0.3, noise_mask = noise_mask, name = name)
+        # if len(sources.keys()) < num_plot_markers:
+        #     num_plot_markers = len(sources.keys()) - 1
+        # plot_points = np.arange(
+        #     0, 
+        #     max(len(sources.keys()) - 10, num_plot_markers),
+        #     max(len(sources.keys())//num_plot_markers, 1)
+        # )
+        # perfs = [self._portion_performance(
+        #     np.argsort(vals_source)[::-1], plot_points, sources=sources)
+        #          for vals_source in vals_sources]
+        # print(len(perfs))
+        # rnd = np.mean([self._portion_performance(
+        #     np.random.permutation(np.argsort(vals_sources[0])[::-1]),
+        #     plot_points, sources=sources) for _ in range(10)], 0)
+        # plt.plot(plot_points/len(self.X) * 100, perfs[0] * 100, 
+        #          '-', lw=5, ms=10, color='b')
+        # if len(vals)==3:
+        #     plt.plot(plot_points/len(self.X) * 100, perfs[1] * 100, 
+        #              '--', lw=5, ms=10, color='orange')
+        #     legends = ['TMC-Shapley ', 'G-Shapley ', 'LOO', 'Random']
+        # elif len(vals)==2:
+        #     legends = ['TMC-Shapley ', 'LOO', 'Random']
+        # else:
+        #     legends = ['TMC-Shapley ', 'Random']
+        # plt.plot(plot_points/len(self.X) * 100, perfs[-1] * 100, 
+        #          '-.', lw=5, ms=10, color='g')
+        # plt.plot(plot_points/len(self.X) * 100, rnd * 100, 
+        #          ':', lw=5, ms=10, color='r')    
+        # plt.legend(legends)
+        # if self.directory is not None and name is not None:
+        #     plt.savefig(os.path.join(
+        #         self.directory, 'plots', '{}.png'.format(name)),
+        #                 bbox_inches = 'tight')
+        #     plt.close()
+
+    def visualize_values_distr_sorted(self, noise_sorted_indices, trsize, portion, noise_mask =None, name=None, poisoned=None):
+        x1, y1, base = [], [], []
+        if poisoned is None:
+            poisoned = int(trsize * portion)
+        print(trsize)
+        for vari in range(10, trsize, 10):
+            if vari < 3000:
+                found = 0
+                #actual_found = 0
+                for i in range(vari):
+                    #print('stt:', i)
+                    index = noise_sorted_indices[i]
+                    if noise_mask[index]:
+                        found += noise_mask[index]
+                #synthetic_found = found - actual_found
+                detection_rate = found / poisoned
+                baseline = vari * 0.3 * 0.8
+                print(f'inspected: {vari}, found: {found}, detection rate: {detection_rate:.2f} baseline ti le tim ra loi co lon hon 0.8 khong: {baseline:.2f}')
+                x1.append(vari)
+                y1.append(found)
+                base.append(vari * poisoned / trsize * 1.0)
+                
+        plt.scatter(x1, y1, s=10, color='red')
+        plt.scatter(x1, base, s=10, color='orange')
+        plt.xlabel('Inspected Images')
+        plt.ylabel('Detected Images')
+        #yticks_values = np.arange(0, 1, 0.1)
+        #plt.yticks(yticks_values)
+        plt.title('Detection vs Gradient Inspection')
+        #plt.show()
+        plt.savefig(os.path.join(
+            self.directory, 'plots', '{}.png'.format(name)),
+            bbox_inches = 'tight')
+        
     def _portion_performance(self, idxs, plot_points, sources=None):
         """Given a set of indexes, starts removing points from 
         the first elemnt and evaluates the new model after
